6dc5ac760ec4b11cbf4e4a54472366f995544e66
Igor9779 <darthhoit97@gmail.com>
2023-06-30 17:20:06 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+  const inputRef = document.querySelector('input');
+  const createBtn = document.querySelector('[data-create]');
+  const deleteBtn = document.querySelector('[data-destroy]');
+  const boxesContainerRef = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', createBoxes);
+deleteBtn.addEventListener('click', deleteBoxes);
+
+function createBoxes() {
+  const amount = Number(inputRef.value);
+  const boxes = [];
+
+  for (const item of amount) {
+    const box = document.createElement('div');
+    const size = 30 + item * 10;
+    box.style.width = size + 'px';
+    box.style.height = size + 'px';
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  };
+
+  boxesContainerRef.append(...boxes);
+};
+
+function deleteBoxes(){
+  boxesContainerRef.innerHTML = '';
+};
